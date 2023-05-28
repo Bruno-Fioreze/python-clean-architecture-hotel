ab193ad4cf8ce21ee8819fdf2376896f2d56e054
Bruno Fioreze <brunofioreze@pop-os.localdomain>
2023-05-27 21:30:37 -0300

    fix BookingDto import

--- booking_service/application/booking/booking_dto.py
+++ booking_service/application/booking/booking_dto.py
@@ -1,17 +1,17 @@
 from datetime import datetime
-from booking_service.domain.booking import entities
-
+from booking_service.domain.booking.entities import Booking
+from booking_service.domain.customers.entities import Customer
 
 class BookingDto(object):
     checkin: datetime
     checkout: datetime
-    customer: entities.Customer
+    customer: Customer
 
-    def __init__(self, checkin:datetime, checkout:datetime, customer: entities.Customer) -> None:
+    def __init__(self, checkin:datetime, checkout:datetime, customer: Customer) -> None:
         self.checkin = checkin
         self.checkout = checkout
         self.customer = customer
 
     def _to_domain(self):
-        return entities.Booking(self.checkin, self.checkout, self.customer)
+        return Booking(self.checkin, self.checkout, self.customer)
     
